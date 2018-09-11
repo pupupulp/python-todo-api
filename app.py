2328cadb772b564903e48380622799ebca9fdc9a
Eagan Martin <mece.martinece@gmail.com>
2018-09-11 13:07:58 +0800

    Added endpoint to get tasks list;

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+tasks = [
+	{
+		'id': 1,
+		'title': u'Python API Development',
+		'description': 'Study different approach and frameworks used to develop API using python',
+		'done': False
+	},
+	{
+		'id': 1,
+		'title': u'GCP Machine Learning Development',
+		'description': 'Study different application and approach on machine learning for finance systems',
+		'done': False
+	},
+]
 
-def index():
-	return "Hello"
+@app.route('/todo/api/v1/tasks', methods = ['GET'])
+
+def get_tasks():
+	return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
 	app.run(debug = True)
