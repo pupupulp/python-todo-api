6cc2c101a0b3399e6f9c80b269266bb6e697dd1f
Eagan Martin <mece.martinece@gmail.com>
2018-09-11 13:35:29 +0800

    Added error handler for 404 requests via make response;

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -29,6 +29,9 @@ def get_task(task_id):
 		abort(404)
 	return jsonify({'task': task[0]})
 
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
 	app.run(debug = True)
